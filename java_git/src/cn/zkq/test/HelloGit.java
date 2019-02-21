a547e4bc25826dfbca99635f40097e829564a7b0
zkq <zkq@itcast.cn>
2019-02-21 21:05:17 +0800

    java_git中的HelloGit中添加一个新方法

--- java_git/src/cn/zkq/test/HelloGit.java
+++ java_git/src/cn/zkq/test/HelloGit.java
@@ -4,4 +4,8 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("你好,Git!!");
     }
+
+    public void test(){
+        System.out.println("你好,java!!");
+    }
 }
